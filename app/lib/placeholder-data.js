f32186c8d5e89324bb97b9f84899dcb55d90c7e6
Maryam Mamdouh <mmamdouh@uplandsoftware.com>
2024-02-12 20:14:42 +0200

    first commit

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,5 +1,6 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
+// dummy data 
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
@@ -8,6 +9,7 @@ const users = [
     password: '123456',
   },
 ];
+
 
 const customers = [
   {
